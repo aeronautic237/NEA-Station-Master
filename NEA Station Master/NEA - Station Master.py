a0c0e8091a7f73b60fcbde8b44ec8009eabfaca7
aeronautic237 <104935332+aeronautic237@users.noreply.github.com>
2022-05-28 22:59:53 +0100

    Modular Research
    The research graphics are now complete and is now modular, using OOP

--- NEA Station Master/NEA - Station Master.py
+++ NEA Station Master/NEA - Station Master.py
@@ -32,6 +32,7 @@ title = pygame.font.SysFont("Sans-serif", 200)
 buttonFont = pygame.font.SysFont("Sans-serif", 100)
 clockTextFont = pygame.font.SysFont("Sans-serif",50)
 clockFont = pygame.font.SysFont("Sans-serif",100)
+normal = pygame.font.SysFont("Sans-serif", 25)
 
 
 #declaring the state of the game - may not be necessary
@@ -102,20 +103,24 @@ def game():
     clockIcon = pygame.image.load("Clock_Icon.jpg")
     clockIcon = pygame.transform.scale(clockIcon, (75, 75))
     screen.blit(clockIcon, (420, 10))
+    #placeholder to pause the clock
     pauseClock = button(darkGrey, [840, 10, 75, 75], "", buttonFont, white, 850, 20)
     pauseIcon = pygame.image.load("pause_Icon.png")
     pauseIcon = pygame.transform.scale(pauseIcon, (70, 70))
     pauseClock.drawButton()
     screen.blit(pauseIcon, (842.5, 12.5))
+    #placeholder to play the clock
     playClock = button(darkGrey, [920, 10, 75,75], "", buttonFont, white, 850, 20)
     playClockIcon = pygame.image.load("play_icon.png")
     playClockIcon = pygame.transform.scale(playClockIcon, (70, 70))
     playClock.drawButton()
     screen.blit(playClockIcon, (922.5, 12.5))
+    #placeholder for the 5X clock speed
     clockX5 = button(darkGrey, [1000, 10, 75,75], "5x", clockTextFont, white, 1020, 30)
     clockX5.drawButton()
     clockX15 = button(darkGrey, [1080, 10, 75, 75], "15x", clockTextFont, white, 1090, 30)
     clockX15.drawButton()
+    #placeholder for the 25X clock speed
     clockX25 = button(darkGrey, [1160, 10, 75, 75], "25x", clockTextFont, white, 1170, 30)
     clockX25.drawButton()
     pygame.draw.rect(screen, pink, [500, 10, 330, 75], 5)
@@ -146,12 +151,18 @@ def game():
                     waiting = False
                     gamestate = "menu"
                     mainMenu()
+            elif RDButton.buttonCoords.collidepoint((pygame.mouse.get_pos())):
+                RDButton.changeButtonColour(pink)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    waiting = False
+                    research()
             elif event.type == pygame.QUIT:
                 waiting = False
                 pygame.quit()
                 sys.exit()
             else:
                 menuButton.changeButtonColour(darkGrey)
+                RDButton.changeButtonColour(darkGrey)
     #train1 = train(white, 1230, 350, 50, 20, -1)
     #train1.drawTrain()
     #time.sleep(1)
@@ -160,6 +171,190 @@ def game():
     #train1.departPlat()
     #train1.destroyTrain()
     #pygame.display.update()
+
+def research():
+
+    
+
+    def drawTree(category):
+        returnButton.changeButtonColour(darkGrey)
+        #This is where we will define the fact files
+
+
+        
+        #draws the tech tree based on which category has been selected
+        #I have labelled the buttons based on what they will say, so it is pretty self- explanatory
+        if category == "facility":
+            pygame.draw.rect(screen, black, [250, 0, 1030, 600])
+            helpPoint = technoButton(darkGrey, [300, 50, 150, 50], "Help Points", normal, white, 325, 65)
+            helpPoint.drawtechButton(100, 0)
+            helpPatrol = technoButton(darkGrey, [550, 50, 150, 50], "Help patrol", normal, white, 575, 65)
+            helpPatrol.drawtechButton(100, 0)
+            bicycle = technoButton(darkGrey, [300, 150, 150, 50], "Bicycle racks", normal, white, 322, 165)
+            bicycle.drawtechButton(100, -100)
+            pAndR = technoButton(darkGrey, [550, 150, 150, 50], "Park and Ride", normal, white, 568, 165)
+            pAndR.drawtechButton(100, 0)
+            timeTable = technoButton(darkGrey, [300, 250, 150, 50], "TimeTables", normal, white, 327, 265)
+            timeTable.drawtechButton(100, -200)
+            DMI = technoButton(darkGrey, [550, 250, 150, 50], "DMIs", normal, white, 605, 265)
+            DMI.drawtechButton(100, 0)
+            LCD = technoButton(darkGrey, [800, 250, 150, 50], "LCDs", normal, white, 855, 265)
+            LCD.drawtechButton(100, 0)
+            toilet = technoButton(darkGrey, [300, 350, 150, 50], "toilets", normal, white, 347, 365)
+            toilet.drawtechButton(100, -300)
+            items = [helpPoint, helpPatrol, bicycle, pAndR, timeTable, DMI, LCD, toilet]
+            pygame.display.update()
+        elif category == "signal":
+            pygame.draw.rect(screen, black, [250, 0, 1030, 600])
+            semaphore = technoButton(darkGrey, [250, 150, 150, 50], "semaphores", normal, white, 275, 165)
+            semaphore.drawtechButton(50, 0)
+            repeater = technoButton(darkGrey, [450, 150, 150, 50], "repeaters", normal, white, 485, 165)
+            repeater.drawtechButton(50, 0)
+            litSems = technoButton(darkGrey, [450, 250, 150, 50], "lit semaphores", normal, white, 465, 265)
+            litSems.drawtechButton(50, -100)
+            threeCols = technoButton(darkGrey, [650, 150, 150, 50], "3-aspect signals", normal, white, 658, 165)
+            threeCols.drawtechButton(50, 0)
+            threeCols.drawtechButton(50, 100)
+            fourCols = technoButton(darkGrey, [850, 150, 150, 50], "4-aspect signals", normal, white, 860, 165)
+            fourCols.drawtechButton(50, 0)
+            TVM = technoButton(darkGrey, [1050, 250, 150, 50], "TVM-430", normal, white, 1090, 265)
+            TVM.drawtechButton(50, -100)
+            ETCS = technoButton(darkGrey, [1050, 150, 150, 50], "ETCS", normal, white, 1100, 165)
+            ETCS.drawtechButton(50, 0)
+            items = [semaphore, repeater, litSems, threeCols, fourCols, TVM, ETCS]
+            pygame.display.update()
+        elif category == "safety":
+            pygame.draw.rect(screen, black, [250, 0, 1030, 600])
+            tripCock = technoButton(darkGrey, [300, 250, 150, 50], "trip-cocks", normal, white, 335, 265)
+            tripCock.drawtechButton(100, 0)
+            AWS = technoButton(darkGrey, [550, 250, 150, 50], "AWS", normal, white, 605, 265)
+            AWS.drawtechButton(100, 0)
+            speedCamera = technoButton(darkGrey, [800, 250, 150, 50], "Speed Cameras", normal, white, 812, 265)
+            speedCamera.drawtechButton(100, 0)
+            speedCamera.drawtechButton(100, 100)
+            TPWS = technoButton(darkGrey, [550, 350, 150, 50], "TPWS", normal, white, 600, 365)
+            TPWS.drawtechButton(100, -100)
+            items = [tripCock, AWS, speedCamera, TPWS,]
+            pygame.display.update()
+        elif category == "comms":
+            pygame.draw.rect(screen, black, [250, 0, 1030, 600])
+            sigBoxBell = technoButton(darkGrey, [300, 350, 150, 50], "Signal box bell", normal, white, 315, 365)
+            sigBoxBell.drawtechButton(100, 0)
+            trackSidePhone = technoButton(darkGrey, [550, 350, 150, 50], "Track-side phones", normal, white, 552, 365)
+            trackSidePhone.drawtechButton(100, 0)
+            inCabPhone = technoButton(darkGrey, [800, 350, 150, 50], "In-Cab phones", normal, white, 815, 365)
+            inCabPhone.drawtechButton(100, 0)
+            GSMR = technoButton(darkGrey, [1050, 350, 150, 50], "GSM-R", normal, white, 1095, 365)
+            GSMR.drawtechButton(100, 0)
+            items = [sigBoxBell, trackSidePhone, inCabPhone, GSMR]
+            pygame.display.update()
+        else:#this one is for the track - just to save memory
+            pygame.draw.rect(screen, black, [250, 0, 1030, 600])
+            threeRail = technoButton(darkGrey, [300, 450, 150, 50], "3rd Rail", normal, white, 342, 465)
+            threeRail.drawtechButton(100, 0)
+            OHLE = technoButton(darkGrey, [550, 450, 150, 50], "OHLE", normal, white, 600, 465)
+            OHLE.drawtechButton(100, 0)
+            OHLEspeed = technoButton(darkGrey, [800, 450, 150, 50], "High speed OHLE", normal, white, 804, 465)
+            OHLEspeed.drawtechButton(100, 0)
+            fourRail = technoButton(darkGrey, [800, 350, 150, 50], "4th Rail", normal, white, 842, 365)
+            fourRail.drawtechButton(100, 100)
+            flangeGreaser = technoButton(darkGrey, [300, 350, 150, 50], "flange-Greasers", normal, white, 311, 365)
+            flangeGreaser.drawtechButton(100, 100)
+            points = technoButton(darkGrey, [300, 250, 150, 50], "points", normal, white, 350, 265)
+            points.drawtechButton(100, 200)
+            checkRail = technoButton(darkGrey, [550, 250, 150, 50], "check-rails", normal, white, 580, 265)
+            checkRail.drawtechButton(100, 0)
+            welding = technoButton(darkGrey, [800, 250, 150, 50], "Welding", normal, white, 840, 265)
+            welding.drawtechButton(100, 0)
+            superElevate = technoButton(darkGrey, [1050, 250, 150, 50], "Super-elevation", normal, white, 1060, 265)
+            superElevate.drawtechButton(100, 0)
+            items = [threeRail, OHLE, OHLEspeed, fourRail, flangeGreaser, points, checkRail, welding, superElevate]
+            pygame.display.update()
+        #this is where we check whether the items are clicked
+        waiting = True
+        while waiting == True:
+            for event in pygame.event.get():
+                i = 0
+                for i in range(len(items)):
+                    if items[i].buttonCoords.collidepoint((pygame.mouse.get_pos())):
+                        items[i].changeButtonColour(pink)
+                        #if event.type == pygame.MOUSEBUTTONUP:
+                            #waiting = False
+                        #insert the fact files here
+                    else:
+                        items[i].changeButtonColour(darkGrey)
+                if event.type == pygame.KEYDOWN:
+                    if (event.key == pygame.K_BACKSPACE):
+                        waiting == False
+                        research()
+                elif event.type == pygame.QUIT:
+                    waiting = False
+                    pygame.quit()
+                    sys.exit()
+                
+    
+    #draws the initial screen for the research tab, drawing the buttons
+    screen.fill(black)
+    pygame.draw.rect(screen, darkGrey, [0, 620, 1280, 100])
+    facility = button(darkGrey, [50, 50, 150, 50], "Station Facilities", normal, white, 55, 65)
+    facility.drawButton()
+    signal = button(darkGrey, [50, 150, 150, 50], "Signalling", normal, white, 80, 165)
+    signal.drawButton()
+    safety = button(darkGrey, [50, 250, 150, 50], "Safety systems", normal, white, 60, 265)
+    safety.drawButton()
+    comms = button(darkGrey, [50, 350, 150, 50], "Communications", normal, white, 55, 365)
+    comms.drawButton()
+    track = button(darkGrey, [50, 450, 150, 50], "Tracks", normal, white, 95, 465)
+    track.drawButton()
+    returnButton = button(menuScreenColour, [1200, 635, 75, 75], "", clockTextFont, white, 1210, 655)
+    returnButton.drawButton()
+    pygame.display.update()
+    #now we check which button has been pressed
+    waiting = True
+    while waiting == True:
+        for event in pygame.event.get():
+            if facility.buttonCoords.collidepoint((pygame.mouse.get_pos())):
+                facility.changeButtonColour(pink)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    #waiting = False
+                    drawTree("facility")
+            elif signal.buttonCoords.collidepoint((pygame.mouse.get_pos())):
+                signal.changeButtonColour(pink)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    #waiting = False
+                    drawTree("signal")
+            elif safety.buttonCoords.collidepoint((pygame.mouse.get_pos())):
+                safety.changeButtonColour(pink)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    #waiting = False
+                    drawTree("safety")
+            elif comms.buttonCoords.collidepoint((pygame.mouse.get_pos())):
+                comms.changeButtonColour(pink)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    #waiting = False
+                    drawTree("comms")
+            elif track.buttonCoords.collidepoint((pygame.mouse.get_pos())):
+                track.changeButtonColour(pink)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    #waiting=False
+                    drawTree("t")
+            elif returnButton.buttonCoords.collidepoint((pygame.mouse.get_pos())):
+                returnButton.changeButtonColour(pink)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    waiting=False
+                    game()
+            elif event.type == pygame.QUIT:
+                waiting = False
+                pygame.quit()
+                sys.exit()
+            else:
+                facility.changeButtonColour(darkGrey)
+                signal.changeButtonColour(darkGrey)
+                safety.changeButtonColour(darkGrey)
+                comms.changeButtonColour(darkGrey)
+                track.changeButtonColour(darkGrey)
+                returnButton.changeButtonColour(menuScreenColour)
+    
 
 #new object for train
 class train:
@@ -250,7 +445,31 @@ class button:
         self.buttonColour = newColour
         self.drawButton()
         pygame.display.update()
+
+#This class is a subclass of button and acts in the research category of buttons
+class technoButton(button):
+
     
+    #define a new drawButton to draw a line with it, with potential extra parameters
+    def drawtechButton(self, xdisplacement, ydisplacement):
+        #draw a rectangle
+        pygame.draw.rect(screen, self.buttonColour, self.buttonPos)
+        #write the text
+        write(self.text, self.font, self.textColour, self.textx, self.texty)
+        #Here we will draw the lines - this is where the extra arameters can be helpful
+        if ydisplacement == 0:
+            #draw a straight line back to the previous button
+            pygame.draw.line(screen, white, [self.buttonPos[0] - xdisplacement, self.buttonPos[1] + ((self.buttonPos[3])/2)], [self.buttonPos[0], self.buttonPos[1] + ((self.buttonPos[3])/2)])
+        else:
+            #draw line from button to midway in the gap
+            pygame.draw.line(screen, white, [self.buttonPos[0] - xdisplacement, self.buttonPos[1] +(self.buttonPos[3]/2) + ydisplacement], [self.buttonPos[0] - (xdisplacement/2), (self.buttonPos[1] + (self.buttonPos[3]/2)) + ydisplacement])
+            #draw line from end of previous line to however far down the new button is
+            pygame.draw.line(screen, white, [(self.buttonPos[0] - (xdisplacement/2)), self.buttonPos[1] + (self.buttonPos[3]/2)], [self.buttonPos[0] - (xdisplacement/2), self.buttonPos[1] + (self.buttonPos[3]/2) + ydisplacement])
+            #draw line from end of previous line to new button
+            pygame.draw.line(screen, white, [(self.buttonPos[0] - (xdisplacement/2)), self.buttonPos[1] + (self.buttonPos[3]/2)], [self.buttonPos[0], self.buttonPos[1] + (self.buttonPos[3]/2)])
+        pygame.display.update()
+
+
 #will print text as the user wishes
 def write(text, font, colour, xpos, ypos):
     font = font
