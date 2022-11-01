b31dd2f77b40351dbbf89d34ea5523016b41cfce
aeronautic237 <104935332+aeronautic237@users.noreply.github.com>
2022-11-01 09:10:19 +0000

    BuildEntry - soooo close!
    I thought I needed a new engine to make the coordinate system work, but it turns out I simply needed to move the line the updates position to the end of the if statement. It works partially now, but still needs some work

--- NEA - Station Master.py
+++ NEA - Station Master.py
@@ -755,13 +755,13 @@ def buildEntry(returnButton):
                 #the above line will check if the cursor is in a buildable area before moving the rectangle.
                 oldPosition = pygame.Rect((positionCoord[0]-(positionCoord[0]%40),positionCoord[1]-(positionCoord[1]%40)),(40,40))
                 positionCoord = pygame.mouse.get_pos()#new position of the mouse
-                storeCoordx, storeCoordy = int(position[0]/1240), int((position[1]/10)-20)# stores the coordinates of the mouse against the .txt grid (idexed from 1
-                pygame.draw.rect(screen, lightGreen, position) # draw a gold box to show where the mouse is.
+                storeCoordx, storeCoordy = int(positionCoord[0]//1240), int(position[1]/40) # stores the coordinates of the mouse against the .txt grid (idexed from 1
+                pygame.draw.rect(screen, lightGreen, position) # draw a green box to show where the mouse is.
                 pygame.display.update()
                 if position.collidepoint((pygame.mouse.get_pos())) == False:
                     print(str(storeCoordy))
                     print(str(storeCoordx))
-                    if entryLayout[storeCoordy][storeCoordx] == "0":
+                    if entryLayout[storeCoordy-10][storeCoordx] == "0":
                         #need to draw a dashed line here
                         #length is 40, so 3 white lines and two black lines should do the trick
                         pygame.draw.rect(screen, black, position)
@@ -774,18 +774,18 @@ def buildEntry(returnButton):
                         pygame.draw.rect(screen, black, position)
                         pygame.draw.line(screen, white, (position[0], position[1] + 20), (position[0] + 40, position[1] + 20))
                         pygame.display.update()
-                    position = pygame.Rect((positionCoord[0]-(positionCoord[0]%40),positionCoord[1]-(positionCoord[1]%40)),(40,40))
-
+            
                 print(storeCoordy)# DEBUG
                 print(storeCoordx)# DEBUG
                 if event.type == pygame.MOUSEBUTTONUP:
                     entryLayout[storeCoordy-5][storeCoordx] = "1"
                     money = money - 2500
                 #check for whether the return button was hovered over/clicked
-            elif returnButton.buttonCoords.collidepoint((pygame.mouse.get_pos())):
+            position = pygame.Rect((positionCoord[0]-(positionCoord[0]%40),positionCoord[1]-(positionCoord[1]%40)),(40,40))
+            if returnButton.buttonCoords.collidepoint((pygame.mouse.get_pos())):
                 returnButton.changeButtonColour(pink)
                 if event.type == pygame.MOUSEBUTTONUP:
-                    with open("saveData/.entryPoints.txt", "w", newline="") as file:
+                    with open("saveData/entryPoints.txt", "w", newline="") as file:
                         writer = csv.writer(file)
                         writer.writerows(entryLayout)
                     shop()
